3caa44c836c8688b0ed16af91140d4d231244da9
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2023-02-08 13:45:07 +0530

    frontend code using React-redux

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import Blogs from "./components/Blogs";
+import UserBlogs from "./components/UserBlogs";
+import BlogDetail from "./components/BlogDetail";
+import AddBlog from "./components/AddBlog";
+import {Routes,Route} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+  return <React.Fragment>
+  <header>
+  <Header/>
+  </header>
+  <main>
+  <Routes>
+  <Route path="/auth"  element={<Auth/>}/> 
+  <Route path="/blogs"  element={<Blogs/>}/> 
+  <Route path="/blogs/add" element={<AddBlog />} />
+  <Route path="/myblogs"  element={<UserBlogs/>}/> 
+  <Route path="/myBlogs/:id" element={<BlogDetail />} />{" "}
+  
+   </Routes>
+  </main>
+  </React.Fragment>
+}
+
+export default App;
